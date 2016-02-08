5aefef078acf8a6d53f1edc66fd8032a6d564607
Daniel <disidrot@gmail.com>
2016-02-07 16:26:02 -0800

    Added NavX required packages

--- 2016Robot/src/org/usfirst/frc/team696/robot/subsystems/Chassis.java
+++ 2016Robot/src/org/usfirst/frc/team696/robot/subsystems/Chassis.java
@@ -5,7 +5,8 @@ import org.usfirst.frc.team696.robot.RobotMap;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.AnalogGyro;
+
+
 /**
  *
  */
@@ -14,7 +15,7 @@ public class Chassis extends Subsystem {
     RobotDrive drive = new RobotDrive(RobotMap.frontLeftMotor, RobotMap.rearLeftMotor, RobotMap.frontRightMotor, RobotMap.rearRightMotor);
 	Encoder leftEncoder = new Encoder(RobotMap.encoderLeftA, RobotMap.encoderLeftB);
 	Encoder rightEncoder = new Encoder(RobotMap.encoderRightA, RobotMap.encoderRightB);
-	AnalogGyro gyro = new AnalogGyro(RobotMap.gyroChannel);
+	
 	
 	double leftSpeed = 0;
 	double rightSpeed = 0;
@@ -22,11 +23,10 @@ public class Chassis extends Subsystem {
 	protected void initDefaultCommand() {}
 	
 	public void resetGyro() {
-		gyro.reset();
 	}
 	
 	public double getAngle() {
-		return gyro.getAngle();
+		return 0;
 	}
 	
 	public void setLeftSpeed(double leftSpeed) {
