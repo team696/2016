60450d0bff147e43c012a9ad726bc839176dc975
Daniel Torres <disidrot@gmail.com>
2016-02-23 18:41:37 -0800

    Added PID.java in org.usfirst.frc.team696.robot
    Will be set with these values in code

---
+++ 2016Robot/src/org/usfirst/frc/team696/robot/PID.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team696.robot;
+
+public class PID {
+	private double kP = 0;
+	private double kI = 0;
+	private double kD = 0;
+	private double alpha = 0;
+	private double error = 0;
+	private double oldError = 0;
+	private double cumulativeError = 0;
+	
+	public PID(double kP, double kI, double kD, double aplpha){
+		this.kP = kP;
+		this.kI = kI;
+		this.kD = kD;
+		this.alpha = alpha;
+	}
+
+	public void setError(double error){
+		oldError = this.error;
+		this.error = error;
+	}
+	
+	public void setPID(double kP, double kI, double kD, double alpha){
+		this.kP = kP;
+		this.kI = kI;
+		this.kD = kD;
+		this.alpha = alpha;
+	}
+	
+	private double P(){
+		return error * kP;
+	}
+	
+	private double I(){
+		cumulativeError *= alpha;
+		cumulativeError += error;
+		return cumulativeError * kI;
+	}
+	
+	private double D(){
+		return (error - oldError) * kD;
+	}
+	
+	public double getValue(){
+		return P() + I() + D();
+	}
+}
