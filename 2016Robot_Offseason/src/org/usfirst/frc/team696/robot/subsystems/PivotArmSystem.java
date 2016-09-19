32eee89223ae59be4fc8cf5b03c6e6fb83f6f4df
FRC696 <FRC696@FRC696-Prog2.gusd.lcl>
2016-09-19 14:24:40 -0700

    Ready to test halleffect emergency stops for pivot in 2016Robot_Offseason

--- 2016Robot_Offseason/src/org/usfirst/frc/team696/robot/subsystems/PivotArmSystem.java
+++ 2016Robot_Offseason/src/org/usfirst/frc/team696/robot/subsystems/PivotArmSystem.java
@@ -7,6 +7,7 @@ import org.usfirst.frc.team696.utilities.PIDControl;
 import org.usfirst.frc.team696.utilities.StallPrevention;
 import org.usfirst.frc.team696.utilities.Util;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.PIDSource;
@@ -28,7 +29,8 @@ public class PivotArmSystem extends Subsystem {
 	StallPrevention topSP = new StallPrevention(30);
 	StallPrevention botSP = new StallPrevention(30);
 	Timer discBreakTimer = new Timer();
-	
+	DigitalInput topLimit = new DigitalInput(19);
+	DigitalInput bottomLimit = new DigitalInput(20);
 	
 	boolean ratchet = false;
 	
@@ -42,6 +44,10 @@ public class PivotArmSystem extends Subsystem {
 	private static double kP = 0.03;
 	
 	PIDControl PID = new PIDControl(kP);
+	
+	double oldPivotEncoder = 0;
+	boolean broken = false;
+	double pivotError = 0;
 	
 	public PivotArmSystem() {
 	}
@@ -68,13 +74,58 @@ public class PivotArmSystem extends Subsystem {
     	if(targetAngle <= 10/*test*/)targetAngle = 11;
     	this.targetAngle = targetAngle;
     	
-//    	if(this.targetAngle < 0)this.targetAngle = Robot.pivotEncoder.get();
-    	
+    	/*
+    	 * decide speed value for pivot
+    	 */
     	error = this.targetAngle - Robot.pivotEncoder.get();
     	error = Util.deadZone(error, -3, 3, 0);
     	PID.setError(error);
     	speed = Util.constrain(PID.getValue(), -Robot.pivotConstrainSpeed, Robot.pivotConstrainSpeed);
     	if(!Robot.endOfMatch)speed = Util.constrain(PID.getValue(), -1, 1);
+    	
+    	/*
+    	 * decide whether encoder is broken on pivot
+    	 * TO BE TESZTED
+    	 */
+    	pivotError = Robot.pivotEncoder.get() - oldPivotEncoder;
+    	if((speed == 0 && pivotError == 0) || (speed > 0 && pivotError > 0) || (speed < 0 && pivotError < 0))broken = false;
+    	else broken = true; 
+    	oldPivotEncoder = Robot.pivotEncoder.get();
+    	
+    	/*
+    	 * what to do if broken 
+    	 * TO BE TESTED
+    	 */
+    	if(broken){
+    		if(this.targetAngle > 50){
+    			if(topLimit.get())speed = 0;
+    			else speed = 0.25;
+    		}
+    		if(this.targetAngle < 50){
+    			if(bottomLimit.get())speed = 0;
+    			else speed = -0.25;
+    		}
+    	}
+    	
+    	/*
+    	 * constrain speed to pivotConstrainSpeed constant
+    	 */
+    	speed = Util.constrain(speed, -Robot.pivotConstrainSpeed, Robot.pivotConstrainSpeed);
+
+    	/*
+    	 * Disc Break Code
+    	 * TO BE TESTED
+    	 */
+//    	if(speed != 0 && discBreakTimer.get() == 0){
+//    		discBreakTimer.start();
+//    		brake(false);
+//    	}
+//    	if(speed == 0){
+//    		discBreakTimer.stop();
+//    		discBreakTimer.reset();
+//    		brake(true);
+//    	}
+//    	if(discBreakTimer.get() < 0.1)speed = 0;//jack
     	
     	//manual control with straight out voltage
 //    	if(error > 0)speed = 0.7;
@@ -88,18 +139,6 @@ public class PivotArmSystem extends Subsystem {
     }
     
     public void run(){
-//    	if(speed != 0 && discBreakTimer.get() == 0){
-//    		discBreakTimer.start();
-//    		brake(false);
-//    	}
-//    	if(speed == 0){
-//    		discBreakTimer.stop();
-//    		discBreakTimer.reset();
-//    		brake(true);
-//    	}
-//    	if(discBreakTimer.get() < 0.1)speed = 0;//jack
-
-    	speed = Util.constrain(speed, -Robot.pivotConstrainSpeed, Robot.pivotConstrainSpeed);
 		topPivotMotor.set(-speed);
 		botPivotMotor.set(-speed);
     }
